74af963a5b8ac2625c0520d435219a91ab13e7ba
nick7v <125310825+nick7v@users.noreply.github.com>
2024-03-16 00:26:36 +0300

    used RxJava

--- app/src/main/java/com/lnick7v/shoppinglist/MainViewModel.kt
+++ app/src/main/java/com/lnick7v/shoppinglist/MainViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
